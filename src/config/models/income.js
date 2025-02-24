0cca277577f95c0b8a4ca1f2ebee13432a81f4e2
sohayla <najibsoula5@gmail.com>
2025-02-24 23:10:43 +0200

    homeRoutes.js

---
+++ src/config/models/income.js
@@ -0,0 +1,13 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Income = sequelize.define('Income', {
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    title: { type: DataTypes.STRING, allowNull: false },
+    description: { type: DataTypes.TEXT },
+    amount: { type: DataTypes.FLOAT, allowNull: false },
+    currency: { type: DataTypes.STRING, allowNull: false },
+    date: { type: DataTypes.DATEONLY, allowNull: false }
+});
+
+module.exports = Income;
